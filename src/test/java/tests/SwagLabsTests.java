b4c580e6104a61afc9d5d409163b323e468b4451
Veljko Andrijasevic <andrijasevicveljko1999@gmail.com>
2023-10-12 22:27:19 +0200

    Add necessary folders and test 'verify the url'

---
+++ src/test/java/tests/SwagLabsTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.SwagLabsRetry;
+
+public class SwagLabsTests extends BasicTest {
+
+        @Test (priority = 1 , retryAnalyzer = SwagLabsRetry.class)
+        public void verifyTheUrlOnTheCartPage () {
+            String username = "standard_user";
+            String password = "secret_sauce";
+
+            loginPage.clearAndTypeUsername(username);
+            loginPage.clearAndTypePassword(password);
+            loginPage.clickOnLoginButton();
+
+            Assert.assertEquals(driver.getCurrentUrl(),
+                    baseUrl + "/inventory.html",
+                    "Should be redirected to inventory page after login.");
+
+            topNavPage.clickOnTheShoppingCartButton();
+            Assert.assertEquals(driver.getCurrentUrl(),
+                    baseUrl + "/cart.html",
+                    "Should be redirected to cart page after click on the cart button.");
+    }
+
+
+
+
+
+
+
+
+}
