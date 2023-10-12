410ede831933ad7687fccc4e1927d9fc368da2e7
Veljko Andrijasevic <andrijasevicveljko1999@gmail.com>
2023-10-12 22:35:37 +0200

    Verify the title page

--- src/test/java/tests/SwagLabsTests.java
+++ src/test/java/tests/SwagLabsTests.java
@@ -6,26 +6,43 @@ import retry.SwagLabsRetry;
 
 public class SwagLabsTests extends BasicTest {
 
-        @Test (priority = 1 , retryAnalyzer = SwagLabsRetry.class)
-        public void verifyTheUrlOnTheCartPage () {
-            String username = "standard_user";
-            String password = "secret_sauce";
+    @Test(priority = 1, retryAnalyzer = SwagLabsRetry.class)
+    public void verifyTheUrlOnTheCartPage() {
+        String username = "standard_user";
+        String password = "secret_sauce";
 
-            loginPage.clearAndTypeUsername(username);
-            loginPage.clearAndTypePassword(password);
-            loginPage.clickOnLoginButton();
+        loginPage.clearAndTypeUsername(username);
+        loginPage.clearAndTypePassword(password);
+        loginPage.clickOnLoginButton();
 
-            Assert.assertEquals(driver.getCurrentUrl(),
-                    baseUrl + "/inventory.html",
-                    "Should be redirected to inventory page after login.");
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/inventory.html",
+                "Should be redirected to inventory page after login.");
 
-            topNavPage.clickOnTheShoppingCartButton();
-            Assert.assertEquals(driver.getCurrentUrl(),
-                    baseUrl + "/cart.html",
-                    "Should be redirected to cart page after click on the cart button.");
+        topNavPage.clickOnTheShoppingCartButton();
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/cart.html",
+                "Should be redirected to cart page after click on the cart button.");
     }
 
+    @Test(priority = 2, retryAnalyzer = SwagLabsRetry.class)
+    public void verifyTheTitlePage() {
+        String username = "standard_user";
+        String password = "secret_sauce";
 
+        loginPage.clearAndTypeUsername(username);
+        loginPage.clearAndTypePassword(password);
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(driver.getCurrentUrl(),
+                baseUrl + "/inventory.html",
+                "Should be redirected to inventory page after login.");
+
+        topNavPage.clickOnTheShoppingCartButton();
+        Assert.assertEquals(driver.getTitle(),
+                "Swag Labs",
+                "Title should be Swag Labs.");
+    }
 
 
 
