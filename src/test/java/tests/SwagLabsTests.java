be351c51fa29bf8d73e80036ca161d1dde4c9cfb
Veljko Andrijasevic <andrijasevicveljko1999@gmail.com>
2023-10-13 11:29:26 +0200

    Verify if the linkedin button is working

--- src/test/java/tests/SwagLabsTests.java
+++ src/test/java/tests/SwagLabsTests.java
@@ -471,4 +471,20 @@ public class SwagLabsTests extends BasicTest {
                 .withMessage("Should be redirected to the sauce labs facebook account")
                 .until(ExpectedConditions.urlToBe("https://www.facebook.com/saucelabs"));
     }
+    @Test (priority = 38, retryAnalyzer = SwagLabsRetry.class)
+    public void verifyIfTheLinkedinButtonIsWorking () throws InterruptedException {
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+        loginPage.clearAndTypeUsername(username);
+        loginPage.clearAndTypePassword(password);
+        loginPage.clickOnLoginButton();
+        topNavPage.clickOnTheShoppingCartButton();
+        footer.clickOnTheLinkedinButton();
+        driver.navigate().to("https://www.linkedin.com/authwall?trk=bf&trkInfo=AQEj5aRtH8HZbAAAAYsfGhVI6YC-QfKXWLJ-7m1J99ClbXh8_eieR5OUzkFtoFzGEi2reCxtY-iYlB0NfBtL6p_hYjfqvdJzwBzea5Zn32baz3wk3evsByzCqzJZOPp5E1EWtX0=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fsauce-labs%2F");
+        wait
+                .withMessage("Should be redirected to the sauce labs facebook account")
+                .until(ExpectedConditions.urlToBe("https://www.linkedin.com/authwall?trk=bf&trkInfo=AQEj5aRtH8HZbAAAAYsfGhVI6YC-QfKXWLJ-7m1J99ClbXh8_eieR5OUzkFtoFzGEi2reCxtY-iYlB0NfBtL6p_hYjfqvdJzwBzea5Zn32baz3wk3evsByzCqzJZOPp5E1EWtX0=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fsauce-labs%2F"));
+    }
+
 }
